c940d446fc2a3e8235552d076f3f460f05c5f2ca
Tarlochan5268 <brotherstarlochan5268@gmail.com>
2020-05-20 19:30:36 -0400

    Made randomied states of image when clicked

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,47 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: BallPage(),
       ),
     );
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Ask Me Anything'),
+        backgroundColor: Colors.blue.shade900,
+      ),
+      body: Container(
+        child: Ball(),
+        color: Colors.blueAccent,
+      ),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int ballNumber = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: FlatButton(
+        onPressed: () {
+          setState(() {
+            ballNumber = Random().nextInt(5) + 1;
+            print('I got Clicked ball is $ballNumber');
+          });
+        },
+        child: Image.asset('images/ball$ballNumber.png'),
+      ),
+    );
+  }
+}
